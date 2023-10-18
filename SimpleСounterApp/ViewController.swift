7ef2979071ac650cebc3e72397d8152b1c984d05
Ivan Malyshev <malyshev.ivan@inbox.ru>
2023-10-18 21:43:41 +0300

    Easy App ready to test on Iphone

--- SimpleСounterApp/ViewController.swift
+++ SimpleСounterApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var LableCounter: UILabel!
+    @IBOutlet weak var ButtonCounter: UIButton!
+    var countVar: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ChangeButtonCounter(_ sender: Any) {
+        countVar += 1
+        LableCounter.text = "Значение счётчика: \(countVar)"
+    }
+    
 }
 
