a4e2461ebd09ce6f885def46605d00c684f3042b
Ivan Malyshev <malyshev.ivan@inbox.ru>
2023-10-21 20:54:29 +0300

    Fix error

--- SimpleСounterApp/ViewController.swift
+++ SimpleСounterApp/ViewController.swift
@@ -1,55 +1,47 @@
-//
-//  ViewController.swift
-//  SimpleСounterApp
-//
-//  Created by Иван Малышев on 18.10.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    var countVar: Int = 0
     func currentDate() -> String{
         let date = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy, HH:mm:ss"
         return (dateFormatter.string(from: date))
     }
-    @IBOutlet weak var LableCounter: UILabel!
-    @IBOutlet weak var ButtonCounter: UIButton!
-    @IBOutlet weak var ButtonCounterDecrease: UIButton!
-    @IBOutlet weak var ResetButtonCounter: UIButton!
+    @IBOutlet weak var labelCounter: UILabel!
+    @IBOutlet weak var buttonCounter: UIButton!
+    @IBOutlet weak var buttonCounterDecrease: UIButton!
+    @IBOutlet weak var resetButtonCounter: UIButton!
     @IBOutlet weak var logHistory: UITextView!
-    var countVar: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func ChangeButtonCounter(_ sender: Any) {
         countVar += 1
-        LableCounter.text = "Значение счётчика: \(countVar)"
-        var currentLog = "\n[\(currentDate())]: значение изменено на +1"
+        labelCounter.text = "Значение счётчика: \(countVar)"
+        let currentLog = "\n[\(currentDate())]: значение изменено на +1"
         logHistory.text += currentLog
     }
     
     @IBAction func ChangeButtonCounterDecrease(_ sender: Any) {
         if countVar - 1 < 0 {
             countVar = 0
-            LableCounter.text = "Значение счётчика: \(countVar)"
-            var currentLog = "\n[\(currentDate())]: попытка уменьшить значение счётчика ниже 0"
+            labelCounter.text = "Значение счётчика: \(countVar)"
+            let currentLog = "\n[\(currentDate())]: попытка уменьшить значение счётчика ниже 0"
             logHistory.text += currentLog
         } else{
             countVar -= 1
-            LableCounter.text = "Значение счётчика: \(countVar)"
-            var currentLog = "\n[\(currentDate())]: значение изменено на -1"
+            labelCounter.text = "Значение счётчика: \(countVar)"
+            let currentLog = "\n[\(currentDate())]: значение изменено на -1"
             logHistory.text += currentLog
         }
         
     }
     @IBAction func ChangeButtonCounterReset(_ sender: Any) {
         countVar = 0
-        LableCounter.text = "Значение счётчика: \(countVar)"
-        var currentLog = "\n[\(currentDate())]: значение сброшено"
+        labelCounter.text = "Значение счётчика: \(countVar)"
+        let currentLog = "\n[\(currentDate())]: значение сброшено"
         logHistory.text += currentLog
     }
 }
